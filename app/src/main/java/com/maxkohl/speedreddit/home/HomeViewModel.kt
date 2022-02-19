8102ac2e080b826b440670233f81f917c046f485
Max Kohl <maxdkohl@gmail.com>
2022-02-19 12:58:38 -0500

    Create HomeFragment with view model

--- app/src/main/java/com/maxkohl/speedreddit/home/HomeViewModel.kt
+++ app/src/main/java/com/maxkohl/speedreddit/home/HomeViewModel.kt
@@ -1,4 +1,37 @@
 package com.maxkohl.speedreddit.home
 
-class HomeViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.maxkohl.speedreddit.data.RedditApi
+import com.maxkohl.speedreddit.data.RedditPost
+import kotlinx.coroutines.launch
+
+class HomeViewModel : ViewModel() {
+
+    private var _redditPostsList = MutableLiveData<List<RedditPost>>()
+    val redditPostsLists: LiveData<List<RedditPost>> = _redditPostsList
+
+    private val _response = MutableLiveData<String>()
+
+    val response: LiveData<String>
+        get() = _response
+
+
+    fun getRedditPostsList() {
+        var postsList: MutableList<RedditPost> = mutableListOf()
+        viewModelScope.launch {
+            try {
+                val response = RedditApi.retrofitService.getAllResponses()
+                response.data.children.forEach { response -> postsList.add(response.data) }
+                _redditPostsList.value = postsList
+                _response.value = "${response.data.children[0].data} RECEIVED"
+            } catch (e: Exception) {
+                _response.value = "Failure: ${e.message}"
+            }
+        }
+    }
+
+
 }
