d55ed637abb630d3936ead4ad759ba68b992fbc3
Max Kohl <maxdkohl@gmail.com>
2022-02-24 15:42:10 -0500

    Added test coverage for Reddit Api Service

---
+++ app/src/test/java/com/maxkohl/speedreddit/RedditApiServiceTest.kt
@@ -0,0 +1,41 @@
+package com.maxkohl.speedreddit
+
+import BaseTest
+import com.maxkohl.speedreddit.data.RedditApiResponse
+import com.maxkohl.speedreddit.data.RedditApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import junit.framework.Assert.assertNotNull
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+class RedditApiServiceTest : BaseTest() {
+    private lateinit var service: RedditApiService
+
+    @Before
+    fun setup() {
+        val url = mockWebServer.url("/")
+        service = Retrofit.Builder().baseUrl(url).addConverterFactory(
+            MoshiConverterFactory.create(
+                Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+            )
+        ).build().create(RedditApiService::class.java)
+    }
+
+    @Test
+    fun api_service() {
+        enqueue("reddit_api_response.json")
+        runBlocking {
+            val apiResponse: RedditApiResponse = service.getAllResponses()
+
+            assertNotNull(apiResponse)
+            assertThat(apiResponse.data.children.size, `is`(4))
+        }
+    }
+}
