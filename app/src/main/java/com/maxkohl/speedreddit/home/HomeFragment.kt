a0433dfee70723b46126e8c526810b8d0d69ba1b
Max Kohl <maxdkohl@gmail.com>
2022-03-02 13:12:41 -0500

    Use API data received in list items

--- app/src/main/java/com/maxkohl/speedreddit/home/HomeFragment.kt
+++ app/src/main/java/com/maxkohl/speedreddit/home/HomeFragment.kt
@@ -12,18 +12,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import coil.compose.rememberImagePainter
 import com.google.accompanist.appcompattheme.AppCompatTheme
 import com.maxkohl.speedreddit.R
 import com.maxkohl.speedreddit.data.RedditPost
@@ -51,15 +53,16 @@ class HomeFragment : Fragment() {
         binding.viewModel = homeViewModel
         binding.postsComposelist.setContent {
             AppCompatTheme {
-                HomeScreen(homeViewModel)
+                HomeScreen(homeViewModel, onNavigate = { handleNav() })
             }
         }
 
         return binding.root
     }
 
-    private fun handleNav(redditPost: RedditPost) {
-        if (!redditPost.mediaType.isNullOrEmpty()) {
+    private fun handleNav() {
+        val redditPost = homeViewModel.redditPost.value
+        if (redditPost != null) {
             when (redditPost.mediaType) {
                 "link" -> {
                     val defaultBrowser = Intent.makeMainSelectorActivity(
@@ -78,51 +81,80 @@ class HomeFragment : Fragment() {
 }
 
 @Composable
-fun HomeScreen(homeViewModel: HomeViewModel) {
+fun HomeScreen(homeViewModel: HomeViewModel, onNavigate: () -> Unit) {
     val redditPosts by homeViewModel.redditPostsLists.observeAsState()
 
-    redditPosts?.let { RedditPostList(it) }
+    redditPosts?.let { RedditPostList(homeViewModel, it, onNavigate) }
 }
 
 @Composable
-fun RedditPostList(redditPosts: List<RedditPost>) {
+fun RedditPostList(
+    homeViewModel: HomeViewModel,
+    redditPosts: List<RedditPost>,
+    onNavigate: () -> Unit
+) {
 
     LazyColumn() {
         items(redditPosts) { redditPost ->
-            RedditListItem(redditPost)
+            RedditListItem(homeViewModel, redditPost, onNavigate)
         }
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun RedditListItem(redditPost: RedditPost) {
+fun RedditListItem(
+    homeViewModel: HomeViewModel,
+    redditPost: RedditPost,
+    onNavigate: () -> Unit
+) {
 
-    Card(Modifier.padding(vertical = 5.dp).fillMaxWidth() ) {
+    Card(
+        onClick = {
+            homeViewModel.onPostClicked(redditPost)
+            onNavigate()
+        }, Modifier
+            .padding(vertical = 5.dp)
+            .fillMaxWidth()
+    ) {
         Row(Modifier.fillMaxWidth()) {
             Image(
-                painter = painterResource(id = R.drawable.reddit_snoo),
-                contentDescription = "Yeehaw",
-                modifier = Modifier.size(60.dp)
+                painter = rememberImagePainter(
+                    data = redditPost.previewImgSrc,
+                    builder = { error(R.drawable.reddit_snoo) }),
+                contentDescription = "Reddit post image",
+                modifier = Modifier
+                    .size(60.dp)
+                    .align(CenterVertically)
             )
-            Column(Modifier.fillMaxWidth()) {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 2.dp)
+            ) {
                 Row() {
-                    Text(text = redditPost.title)
+                    Text(text = redditPost.title, maxLines = 2, overflow = TextOverflow.Ellipsis)
                 }
                 Spacer(Modifier.height(20.dp))
                 Row() {
-                    Text(text = redditPost.subreddit, textAlign = TextAlign.Start)
-                    Spacer(Modifier.width(20.dp))
-                    Text(text = redditPost.upvoteCount, textAlign = TextAlign.End)
+                    Text(text = redditPost.subreddit, fontSize = 14.sp)
+                    Spacer(Modifier.width(60.dp))
+                    Text(
+                        text = redditPost.upvoteCount,
+                        Modifier.weight(1f),
+                        textAlign = TextAlign.End,
+                        fontSize = 14.sp
+                    )
                 }
             }
         }
     }
 }
 
-@Preview
-@Composable
-fun RedditPostListPreview() {
-    val redditPost = listOf<RedditPost>()
-
-    RedditPostList(redditPost)
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun RedditPostListPreview() {
+//    val redditPost = listOf<RedditPost>()
+//
+//    RedditPostList(redditPost)
+//}
