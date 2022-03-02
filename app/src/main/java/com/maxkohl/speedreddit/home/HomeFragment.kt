15a97afe4eb02d39279b78f6b79a02ec21784f20
Max Kohl <maxdkohl@gmail.com>
2022-03-02 09:41:15 -0500

    Begin converting HomeList recycler view into Composable

--- app/src/main/java/com/maxkohl/speedreddit/home/HomeFragment.kt
+++ app/src/main/java/com/maxkohl/speedreddit/home/HomeFragment.kt
@@ -7,8 +7,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.google.accompanist.appcompattheme.AppCompatTheme
 import com.maxkohl.speedreddit.R
 import com.maxkohl.speedreddit.data.RedditPost
 import com.maxkohl.speedreddit.databinding.FragmentHomeBinding
@@ -33,10 +49,11 @@ class HomeFragment : Fragment() {
 
         binding.lifecycleOwner = this
         binding.viewModel = homeViewModel
-        binding.postsRecyclerview.adapter = HomeListAdapter(RedditPostListener { redditPost ->
-            homeViewModel.onPostClicked(redditPost)
-            handleNav(redditPost)
-        })
+        binding.postsComposelist.setContent {
+            AppCompatTheme {
+                HomeScreen(homeViewModel)
+            }
+        }
 
         return binding.root
     }
@@ -58,4 +75,54 @@ class HomeFragment : Fragment() {
             }
         }
     }
+}
+
+@Composable
+fun HomeScreen(homeViewModel: HomeViewModel) {
+    val redditPosts by homeViewModel.redditPostsLists.observeAsState()
+
+    redditPosts?.let { RedditPostList(it) }
+}
+
+@Composable
+fun RedditPostList(redditPosts: List<RedditPost>) {
+
+    LazyColumn() {
+        items(redditPosts) { redditPost ->
+            RedditListItem(redditPost)
+        }
+    }
+}
+
+@Composable
+fun RedditListItem(redditPost: RedditPost) {
+
+    Card(Modifier.padding(vertical = 5.dp).fillMaxWidth() ) {
+        Row(Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(id = R.drawable.reddit_snoo),
+                contentDescription = "Yeehaw",
+                modifier = Modifier.size(60.dp)
+            )
+            Column(Modifier.fillMaxWidth()) {
+                Row() {
+                    Text(text = redditPost.title)
+                }
+                Spacer(Modifier.height(20.dp))
+                Row() {
+                    Text(text = redditPost.subreddit, textAlign = TextAlign.Start)
+                    Spacer(Modifier.width(20.dp))
+                    Text(text = redditPost.upvoteCount, textAlign = TextAlign.End)
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RedditPostListPreview() {
+    val redditPost = listOf<RedditPost>()
+
+    RedditPostList(redditPost)
 }
